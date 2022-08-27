5f901e33b4261853593d4524afa9fb4e25734cc2
Jack <jpick329@gmail.com>
2022-08-27 13:57:07 -0500

    made method public for testing purposes

--- src/main/java/com/jackbets/mybets/wager/WagerNotFoundException.java
+++ src/main/java/com/jackbets/mybets/wager/WagerNotFoundException.java
@@ -2,7 +2,7 @@ package com.jackbets.mybets.wager;
 
 public class WagerNotFoundException extends RuntimeException {
 
-    WagerNotFoundException(Long id) {
+    public WagerNotFoundException(Long id) {
         super("Could not find wager " + id);
     }
 
