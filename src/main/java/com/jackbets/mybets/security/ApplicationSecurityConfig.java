6876836d831c5bc4b10e4e31accbbc9b76fec05e
Jack <jpick329@gmail.com>
2022-09-07 14:53:50 -0500

    disable csrf

--- src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
@@ -28,6 +28,7 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .authorizeHttpRequests((authz) -> authz
                 .anyRequest().authenticated()
             )
