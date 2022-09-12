0417a2473b6b6c26f1bc4d7e0b66346988d42cbc
Jack <jpick329@gmail.com>
2022-09-11 21:15:01 -0500

    created getGrantedAuthorities()

--- src/main/java/com/jackbets/mybets/security/ApplicationUserRole.java
+++ src/main/java/com/jackbets/mybets/security/ApplicationUserRole.java
@@ -1,6 +1,9 @@
 package com.jackbets.mybets.security;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.google.common.collect.Sets;
 
@@ -13,6 +16,14 @@ public enum ApplicationUserRole {
 
     private ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
     }
     
 }
