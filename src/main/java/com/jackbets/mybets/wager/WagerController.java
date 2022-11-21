b742ea6689581ffc2ec81109203c0f94c192b4b1
Jack <jpick329@gmail.com>
2022-11-20 21:07:48 -0600

    update api for thyme template

--- src/main/java/com/jackbets/mybets/wager/WagerController.java
+++ src/main/java/com/jackbets/mybets/wager/WagerController.java
@@ -3,22 +3,20 @@ package com.jackbets.mybets.wager;
 import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jackbets.mybets.response.Response;
 import com.jackbets.mybets.status.Status;
 
 @RestController
-@RequestMapping(path = "api/v1/wager")
 public class WagerController {
 
     private final WagerService wagerService;
@@ -27,24 +25,27 @@ public class WagerController {
         this.wagerService = wagerService;
     }
 
-    @GetMapping
-    public List<Wager> getWagers() {
-        return wagerService.getWagers();
+    @GetMapping({"/wagerlist", "/"})
+    public ModelAndView getAllWagers() {
+        List<Wager> wagers = wagerService.getWagers();
+        ModelAndView modelAndView = new ModelAndView("list-wagers");
+        modelAndView.addObject("wagers", wagers);
+        return modelAndView;
     }
 
-    @PostMapping()
+    @PostMapping(path = "api/v1/wager")
     @PreAuthorize("hasAuthority('bet:write')")
     public Response placeNewWager(@RequestBody Wager wager) {
         return wagerService.addNewWager(wager);
     }
 
-    @DeleteMapping("{wagerId}")
+    @DeleteMapping(path = "api/v1/wager/{wagerId}")
     @PreAuthorize("hasAuthority('bet:write')")
     public void deleteWager(@PathVariable("wagerId") Long wagerId) {
         wagerService.deleteWager(wagerId);
     }
 
-    @PutMapping(path = "/{wagerId}")
+    @PutMapping(path = "api/v1/wager/{wagerId}")
     @PreAuthorize("hasAuthority('bet:write')")
     public void updateWager(@PathVariable("wagerId") Long wagerId, @RequestParam("status") Status status) {
         wagerService.updateWager(wagerId, status);
