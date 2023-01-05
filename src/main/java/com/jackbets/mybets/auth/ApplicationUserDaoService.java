cca33295d9d420e2b92c98c9ff48087833408d94
Jack Pickus <jpick329@gmail.com>
2023-01-05 17:11:50 -0600

    implementation for UserDaoService

--- src/main/java/com/jackbets/mybets/auth/ApplicationUserDaoService.java
+++ src/main/java/com/jackbets/mybets/auth/ApplicationUserDaoService.java
@@ -2,14 +2,23 @@ package com.jackbets.mybets.auth;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("postgresbets")
-public class ApplicationUserDaoService implements ApplicationUserDao{
+public class ApplicationUserDaoService implements ApplicationUserDao {
+
+    private final ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    public ApplicationUserDaoService(ApplicationUserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
 
     @Override
     public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
-        return Optional.empty();
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+        return Optional.ofNullable(applicationUser);
     }
     
 }
