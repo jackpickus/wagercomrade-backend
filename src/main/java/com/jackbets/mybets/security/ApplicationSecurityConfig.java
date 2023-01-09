25beac3ac5b5ec7da5709b154c41b67e43a78f63
Jack Pickus <jpick329@gmail.com>
2023-01-09 16:17:38 -0600

    removed in memory user

--- src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
@@ -9,10 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -66,19 +62,4 @@ public class ApplicationSecurityConfig {
         return provider;
     }
 
-    // @Bean
-    // protected UserDetailsService userDetailsService() {
-
-    //     UserDetails admin = User.builder()
-    //         .username("admin")
-    //         .password(passwordEncoder.encode("setOverSet800"))
-    //         .authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
-    //         .build();
-
-
-    //     return new InMemoryUserDetailsManager(
-    //         admin
-    //     );
-    // }
-    
 }
