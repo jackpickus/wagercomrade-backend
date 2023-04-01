b14fc69ee28633679afde5f63a814629f877ac07
Jack Pickus <jpick329@gmail.com>
2023-03-31 23:25:50 -0500

    add category id

--- src/main/java/com/jackbets/mybets/category/Category.java
+++ src/main/java/com/jackbets/mybets/category/Category.java
@@ -1,5 +1,12 @@
 package com.jackbets.mybets.category;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public enum Category {
 
     NBA("NBA"),
@@ -7,9 +14,15 @@ public enum Category {
     MLB("MLB"),
     NHL("NHL"),
     COLLEGE_FOOTBALL("College Football"),
-    COLLEGE_BASKETBALL("College Basketball");
+    COLLEGE_BASKETBALL("College Basketball"),
+    OTHER("Other");
 
     private final String category;
+    
+    @Id
+    @SequenceGenerator(name = "category_sequence", sequenceName = "category_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+    private Long id;
 
     Category (final String category) {
         this.category = category;
