ac4eca3e9769ceaa06087d49dd49cef926219532
Jack Pickus <jpick329@gmail.com>
2023-06-03 13:02:49 -0500

    category converter

---
+++ src/main/java/com/jackbets/mybets/category/CategoryConverter.java
@@ -0,0 +1,31 @@
+package com.jackbets.mybets.category;
+
+import java.util.stream.Stream;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CategoryConverter implements AttributeConverter<Category, String>{
+
+    @Override
+    public String convertToDatabaseColumn(Category category) {
+        if (category == null) {
+            return null;
+        }
+        return category.getSport();
+    }
+
+    @Override
+    public Category convertToEntityAttribute(String sport) {
+        if (sport == null) {
+            return null;
+        }
+
+        return Stream.of(Category.values())
+        .filter(cat -> cat.getSport().equals(sport))
+        .findFirst()
+        .orElseThrow(IllegalArgumentException::new);
+    }
+
+}
