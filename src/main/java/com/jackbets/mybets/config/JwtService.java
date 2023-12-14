f38a2fd1cdacbfb79814d075b31fa0475829de0d
Jack Pickus <jpick329@gmail.com>
2023-12-14 16:45:24 -0600

    finish up jwt service class

--- src/main/java/com/jackbets/mybets/config/JwtService.java
+++ src/main/java/com/jackbets/mybets/config/JwtService.java
@@ -44,6 +44,19 @@ public class JwtService {
         .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllCliams(String token) {
         return Jwts
             .parser()
