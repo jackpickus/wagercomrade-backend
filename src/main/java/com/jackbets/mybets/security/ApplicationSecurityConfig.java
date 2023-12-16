f841b11074f8d5e9920302e49cb42b20494cb900
Jack Pickus <jpick329@gmail.com>
2023-12-16 11:43:44 -0600

    add session manager to security chain

--- src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jackbets/mybets/security/ApplicationSecurityConfig.java
@@ -5,17 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jackbets.mybets.auth.ApplicationUserService;
+import com.jackbets.mybets.config.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,14 +32,21 @@ public class ApplicationSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
     private final ApplicationUserService applicationUserService;
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .cors(withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
-                    .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                                .anyRequest().authenticated()
                 )
+                .sessionManagement(management -> management
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(withDefaults());
 
         return http.build();
