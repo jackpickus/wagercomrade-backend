ccca111a45466d3a0509704c2fe37fe79bfb8dd3
Jack Pickus <jpick329@gmail.com>
2023-12-30 12:12:12 -0600

    set mail properties

--- src/main/java/com/jackbets/mybets/mail/MailConfig.java
+++ src/main/java/com/jackbets/mybets/mail/MailConfig.java
@@ -1,5 +1,7 @@
 package com.jackbets.mybets.mail;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,7 +12,19 @@ public class MailConfig {
 
     @Bean
     public JavaMailSender getJavaMailSender() {
-        JavaMailSender mailSender = new JavaMailSenderImpl();
+        var mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("email-smtp.us-east-2.amazonaws.com");
+        mailSender.setPort(25);
+
+        mailSender.setUsername("username");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
         return mailSender;
     }
     
