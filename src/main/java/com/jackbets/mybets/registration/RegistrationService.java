2f35b719e5960229409f65eb60cc014fed28af2e
Jack Pickus <jpick329@gmail.com>
2023-12-31 16:16:26 -0600

    add username to registration response

--- src/main/java/com/jackbets/mybets/registration/RegistrationService.java
+++ src/main/java/com/jackbets/mybets/registration/RegistrationService.java
@@ -56,6 +56,7 @@ public class RegistrationService {
 
         return AuthenticationResponse.builder()
             .token(jwtToken)
+            .username(appUser.getUsername())
             .build();
 
     }
