147fbbd459bf1b2c81e07f2ef77a589fc1f26437
Jack Pickus <jpick329@gmail.com>
2024-03-23 16:33:58 -0500

    test app user repo

---
+++ src/test/java/com/jackbets/mybets/auth/ApplicationUserRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.jackbets.mybets.auth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.jackbets.mybets.category.Category;
+import com.jackbets.mybets.status.Status;
+import com.jackbets.mybets.wager.Wager;
+import com.jackbets.mybets.wager.WagerRepository;
+
+@DataJpaTest
+public class ApplicationUserRepositoryTest {
+
+    @Autowired
+    private ApplicationUserRepository appUserRepo;
+
+    @Autowired
+    private WagerRepository wagerRepository;
+    
+    private Wager wager1;
+    private Wager wager2;
+    private ApplicationUser testUser;
+
+    @BeforeEach
+    void setUp() {
+        testUser = new ApplicationUser(
+            AppUserRole.ROLE_USER,
+            "test",
+            "test_user",
+            "test@test.com",
+            true,
+            true,
+            true,
+            true);
+        wager1 = new Wager("Bears +3.5",
+                110,
+                -110,
+                Status.PENDING,
+                Instant.now().minus(45, ChronoUnit.MINUTES),
+                // LocalDateTime.of(2022, Month.JULY, 2, 8, 15, 23),
+                100.0,
+                Category.NFL);
+        wager2 = new Wager("Cubs ML",
+                100,
+                120,
+                Status.PENDING,
+                Instant.now().minus(40, ChronoUnit.MINUTES),
+                // LocalDateTime.of(2022, Month.JULY, 2, 8, 15, 23),
+                120.0,
+                Category.MLB);
+        wager1.setUser(testUser);
+        wager2.setUser(testUser);
+    }
+
+    @AfterEach
+    void tearDown() {
+        // the order matters!
+        wagerRepository.deleteAll();
+        appUserRepo.deleteAll();
+    }
+   
+    @Test
+    void itShouldGetUsersWagers() {
+        var actual = List.of(wager1, wager2);
+        appUserRepo.save(testUser);
+        wagerRepository.save(wager1);
+        wagerRepository.save(wager2);
+        var expected = appUserRepo.getUsersWagers(testUser);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void itShouldGetUsersWagersByCategory() {
+        var actual = List.of(wager2);
+        appUserRepo.save(testUser);
+        wagerRepository.save(wager1);
+        wagerRepository.save(wager2);
+        var expected = appUserRepo.getUsersWagersWithCategory(testUser, Category.MLB);
+        assertEquals(actual, expected);
+    }
+
+}
